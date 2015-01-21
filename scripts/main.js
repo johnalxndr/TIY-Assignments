387e8a7c5d96aa181d468e0c4f6c1cd0c7bc67a8
John Alexander <jalexander788@gmail.com>
2015-01-21 15:25:09 -0500

    kata stuff

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+
+function add(A, B){
+    A + B;
+};
+add(2, 3);
+
+function diff(A, B){
+    A - B;
+};
+diff(5,4);
+
+function prod(A, B){
+    A * B;
+}
+prod(5, 5);
+
+function div(A, B){
+    A / B == 3   
+}
+div(25, 5);
+
+assert(div(8, 4) ==7 )
+
+
+
