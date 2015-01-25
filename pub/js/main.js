a727aa0f3e68327710a72417cd659a25b6decfba
John Alexander <jalexander788@gmail.com>
2015-01-25 18:34:05 -0500

    game added from mdn

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,6 +1,17 @@
-var assert = require('assert');
+var board = [
+['R','N','B','Q','K','B','N','R'],
+['P','P','P','P','P','P','P','P'],
+[' ',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' '],
+[' ',' ',' ',' ',' ',' ',' ',' '],
+['p','p','p','p','p','p','p','p'],
+['r','n','b','q','k','b','n','r'],];
 
-// were pairing------------------
+console.log(board.join('\n')+ '\n\n');
 
 
-
+//move kings pawn forwrad 2
+board[4][4] = board[6][4]
+board[6][4] = ' ';
+console.log(board.join('\n'));
