bfe43b0f264c8581024e3344c0c27de398ba9ac2
John Alexander <jalexander788@gmail.com>
2015-01-25 18:52:41 -0500

    removed crappy statement, added function

--- pub/js/main.js
+++ pub/js/main.js
@@ -8,11 +8,15 @@ var board = [
 ['p','p','p','p','p','p','p','p'],
 ['r','n','b','q','k','b','n','r'],];
 
-console.log(board.join('\n')+ '\n\n');
+
 
 
 //move kings pawn forward
-board[4][4] = board[6][4]
-board[6][4] = ' ';
 
-console.log(board.join('\n'));
+var printBoard=function(){
+    console.log(board.join('\n')+ '\n\n');
+};
+
+printBoard();
+
+
