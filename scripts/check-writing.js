fc28c7463e23a4c75067390f7f3b7384b553be90
John Alexander <jalexander788@gmail.com>
2015-01-25 17:14:39 -0500

    @jaredstevick, check writing, @johnalxndr

--- scripts/check-writing.js
+++ scripts/check-writing.js
@@ -60,3 +60,134 @@ it('should take 9 and return "nine"', function(){
 it('should take 10 and return "ten"', function(){
     assert.equal(toEnglish(10), "ten");
 })
+
+var assert = require('assert');
+
+/*function toEnglish (a) {
+    return "one";
+}
+it('should return 1 as string "one"', function() {
+    assert.equal(toEnglish(1), "one");
+})*/
+
+//function toEnglish(a) {
+//    if (a == 2) {
+//        return "two";
+//    }
+//
+//    else {
+//        return "this is not the number 2";
+//    }
+//}
+//
+//console.log(toEnglish(2));
+//
+//it('should return "two" when 2 is entered and "not two" if any other number is entered', function () {
+//    assert.equal(toEnglish(2), "two");
+//
+//} )
+
+//function toEnglish (a) {
+//    if (a == 3) {
+//        return "three";
+//    } else {
+//        return "this is not the number 3";
+//    }
+//}
+//console.log(toEnglish(3));
+//
+//it('should return "three" when 3 is entered and "not the number 3" if any other number is entered', function() {
+//    assert.equal(toEnglish(3), "three");
+//
+//})
+
+//function toEnglish (a) {
+//    if (a == 4) {
+//    return "four";
+//    } else {
+//        return "this is not the number 4";
+//    }
+//}
+//console.log(toEnglish(4));
+//it('should return "four" when 4 is entered and "not the number 4" if any other number is entered', function() {
+//    assert.equal(toEnglish(4), "four");
+//})
+//
+//
+//function toEnglish (a) {
+//    if (a == 5){
+//        return "five";
+//    } else {
+//        return "this is not the number 5";
+//    }
+// }
+//console.log(toEnglish(5));
+//
+//it('should return "five" when 5 is entered and "not the number 5" if any other number is entered', function() {
+//    assert.equal(toEnglish(5), "five");
+//})
+//
+//function toEnglish (a) {
+//    if (a == 6) {
+//        return "six";
+//    } else {
+//        return "this is not the number 6";
+//    }
+//}
+//console.log(toEnglish(6));
+//
+//it('should return "six" when 6 is entered and "not the number 6" if any other number is entered', function() {
+//    assert.equal(toEnglish(6), "six");
+//})
+//
+//function toEnglish (a) {
+//    if (a == 7) {
+//        return "seven";
+//    } else {
+//        return "this is not the number 7";
+//    }
+//}
+//console.log(toEnglish(7));
+//
+//it('should return "six" when 7 is entered and "not the number 7" if any other number is entered', function() {
+//    assert.equal(toEnglish(7), "seven");
+//})
+//
+//function toEnglish (a) {
+//    if (a == 8) {
+//        return "eight";
+//    } else {
+//        return "this is not the number 8";
+//    }
+//}
+//console.log(toEnglish(8));
+//
+//it('should return "eight" when 8 is entered and "not the number 8" if any other number is entered', function() {
+//    assert.equal(toEnglish(8), "eight");
+//})
+//function toEnglish (a) {
+//    if (a == 9) {
+//        return "nine";
+//    } else {
+//        return "this is not the number 9";
+//    }
+//}
+//console.log(toEnglish(9));
+//
+//it('should return "nine" when 9 is entered and "not the number 9" if any other number is entered', function() {
+//    assert.equal(toEnglish(9), "nine");
+//})
+
+
+function toEnglish (a) {
+    if (a == 10) {
+        return "ten";
+    } else {
+        return "this is not the number ten";
+    }
+}
+console.log(toEnglish(10));
+
+it('should return "ten" when 10 is entered and "not the number 10" if any other number is entered', function() {
+    assert.equal(toEnglish(10), "ten");
+}
