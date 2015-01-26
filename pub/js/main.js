f5bdb4601aa45982ae717e238aec04c0f12f0a41
John Alexander <jalexander788@gmail.com>
2015-01-25 23:29:13 -0500

    made piece move using function, very cool

--- pub/js/main.js
+++ pub/js/main.js
@@ -14,9 +14,17 @@ var board = [
 //move kings pawn forward
 
 var printBoard=function(){
-    console.log(board.join('\n')+ '\n\n');
-};
+    console.log(board.join('\n')+ '\n\n');};
+
+function move(toY, toX, fromY,fromX){
+    toX=toX -1;
+    toY=toY -1;
+    fromX=fromX -1;
+    fromY=fromY -1;
+    board[toY][toX]=board[fromY][fromX];
+    board[fromY][fromX] = ' ';
 
 printBoard();
+}
 
-
+move(5,5,7,5)
