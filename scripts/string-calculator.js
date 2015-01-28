82a171617deae6de7eef125699bcecd61b1ba6fd
John Alexander <jalexander788@gmail.com>
2015-01-27 23:45:22 -0500

    string-calculator with dictionary lookups instead of if else

--- scripts/string-calculator.js
+++ scripts/string-calculator.js
@@ -1,8 +1,49 @@
 var assert  = require('assert');
 
-function plus(A, B){
-   return 10;
-}
+console.log(Date());
+
+
+function fromEnglish(A){
+var numbers = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4, 
+    'five': 5, 
+    'six': 6,
+    'seven': 7, 
+    'eight':8, 
+    'nine': 9,
+    'ten':10,
+    'eleven': 11, 
+    'twelve': 12,
+    'thirteen':13,
+    'fourteen':14,
+    'fifteen': 15, 
+    'sixteen':16,
+    'seventeen':17,
+    'eighteen':18, 
+    'nineteen':19,
+    'twenty':20,
+};
+
+return numbers [A];
+};
+
+console.log(fromEnglish("zero") + fromEnglish("zero"))
+console.log(fromEnglish("one")  + fromEnglish("one"))
+console.log(fromEnglish("two")  + fromEnglish("two"))
+console.log(fromEnglish("three") + fromEnglish("three"))
+console.log(fromEnglish("four") + fromEnglish("four"))
+console.log(fromEnglish("five") + fromEnglish("five"))
+console.log(fromEnglish("six") + fromEnglish("six"))
+console.log(fromEnglish("seven") + fromEnglish("seven"))
+console.log(fromEnglish("eight") + fromEnglish("eight"))
+console.log(fromEnglish("nine") + fromEnglish("nine"))
+console.log(fromEnglish("ten") + fromEnglish("ten"))
+
+
 
 //it('should add "zero" and "one"',function(){
 //    assert.equal(plus("zero", "one"), 1);
@@ -32,6 +73,6 @@ function plus(A, B){
 //it('should add "zero" and "nine"', function(){
 //    assert.equal(plus("zero", "nine"), 9);
 //});
-it('should add "zero" and "ten"', function(){
-    assert.equal(plus("zero", "ten"),10);
-});
+//it('should add "zero" and "ten"', function(){
+//    assert.equal(plus("zero", "ten"),10);
+//});
