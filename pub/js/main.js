31ac23f087071e933351365abec6edfa8a5ccd9a
John Alexander <jalexander788@gmail.com>
2015-02-07 16:07:07 -0500

    trying to get this comment shit to work

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function(){
+    
+$('button').click(function makewords(){
+   
+$.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/254/comments"?access_key=8299cf4d40242d94cd36eee9a54558bb15d1220e", JSON.stringify ({"body": $("textbox").val()}, $("textbox").val(""));    
+})    
+});
