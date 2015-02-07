d6b36df9a11be74732aaecbbce3478098525d54d
John Alexander <jalexander788@gmail.com>
2015-02-07 17:08:10 -0500

    yay my comments button works with api

--- pub/js/main.js
+++ pub/js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     
 $('button').click(function makewords(){
    
-$.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/254/comments"?access_key=8299cf4d40242d94cd36eee9a54558bb15d1220e", JSON.stringify ({"body": $("textbox").val()}, $("textbox").val(""));    
+$.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/254/comments?access_token=8299cf4d40242d94cd36eee9a54558bb15d1220e", JSON.stringify ({"body": $("#textbox").val()}), $("#textbox").val(""));    
 })    
 });
