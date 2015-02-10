6abdd2281d0ffa4c39532fafdf2c0c0e3c9a75cc
John Alexander <jalexander788@gmail.com>
2015-02-09 21:24:44 -0500

    the font is boring but yay color hex clock

--- colorclock/js/main.js
+++ colorclock/js/main.js
@@ -1,1 +1,19 @@
+//lets display the current time
+var d,h,m,s,color; 
+    
+    function myTime(){
+        d = new Date();
+        h = d.getHours();
+        m = d.getMinutes();
+        s = d.getSeconds();
+        
+        color= '#' + h + m + s;
+        
+        document.body.style.background = color;
+        
+        document.getElementById("clock").innerHTML = color;   
+        
+        setTimeout(myTime, 1000);
+    }
 
+    myTime();
