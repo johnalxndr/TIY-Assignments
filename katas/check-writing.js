9602dd8c17fd50e18a235251d9add92201db12e3
John Alexander <jalexander788@gmail.com>
2015-02-14 20:26:28 -0500

    done on this for a while

--- katas/check-writing.js
+++ katas/check-writing.js
@@ -31,7 +31,10 @@ function toEnglish(A){
 it('should take 10 and return ten',function(){
     assert.equal(toEnglish(10),"ten");
 });
-it('should take .50')
+it('should take .50 turn to 50/100', function(){
+    assert.equal(toEnglish(.50), 50/100);
+});
+
 
 console.log(toEnglish(10));
 console.log(toEnglish(11));   
