4c6e35020f66f7c3f794a4293617bed218e80e38
John Alexander <jalexander788@gmail.com>
2015-02-14 23:43:59 -0500

    forgot a few commits on these but got total push events and pull requests as well as avg per day of each

--- apis/gymnastics.js
+++ apis/gymnastics.js
@@ -1,19 +1,48 @@
 var assert = require('assert');
 var events = require('../apis/github/users/johnalxndr/events.json');
 var _ = require('lodash');
+//
 
 var pushEvents = events.filter(function(item){
-    return item.type == 'PushEvent'
+    return item.type == 'PushEvent';
 });
-var otherEvents = events.filter(function(item){
-    return item.type == 'PushEvent'
+var deleteEvents = events.filter(function(item){    
+    return item.type == 'DeleteEvent';
 });
+var pullRequests = events.filter(function(item){
+    return item.type == 'PullRequestEvent';
+}); 
 
 
+function createdDate(a){
+    return  _.map(a, 'created_at')
+};
+//thanks for this one taylor and for the help
+function datediff(a, b){
+    return _.first(a).substring(0,10).localeCompare(_.last(b).substring(0,10)) + 1;
+};
+
+function avg (item){
+    var diff = datediff(createdDate(events), createdDate(events));
+    return item.length / diff
+};
 
 
+function theAnswer(){
+    return{
+        'pushEvents':{
+            'total': pushEvents.length,
+            'perDay':avg(pushEvents)
+    },
+        'pullRequests':{
+            'total':pullRequests.length,
+            'perDay':avg(pullRequests)
+        },
+};
+};
 
-
-
-console.log(pushEvents.length)
-console.log(otherEvents.lenth)
+console.log(pushEvents.length);
+console.log(deleteEvents.length);
+console.log(pullRequests.length);
+console.log(createdDate(events));
+console.log(theAnswer(events));
